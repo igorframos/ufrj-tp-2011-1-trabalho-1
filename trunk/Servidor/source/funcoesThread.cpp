f1f56593ca18310881f44e1127582889fa0c489d
igor.bread <igor.bread@15e5f30d-ea52-5471-b173-e5752f37cba1>
2011-05-13 04:52:19 +0000

    Adicionei mais comentários. Adicionei também alguns trechos de código referentes ao envio de mensagens e ao recebimento de conexões. Talvez eu esteja enrolando para entrar de fato na parte complicada de fazer o multicast e coisa e tal, mas, como não tenho certeza, vou me dar o benefício da dúvida. =P

--- trunk/Servidor/source/funcoesThread.cpp
+++ trunk/Servidor/source/funcoesThread.cpp
@@ -23,6 +23,8 @@
 #include <sys/msg.h>
 #include <sys/ipc.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #include "../headers/cliente"
 #include "../headers/mensagem"
@@ -30,16 +32,36 @@
 
 namespace funcoesThread
 {
+    /*
+     *
+     * Thread que envia as mensagens para os clientes.
+     *
+     * Fica verificando se a lista de mensagens está vazia. Caso não esteja, envia todas as mensagens que estiverem
+     * aguardando na fila.
+     *
+     */
     void *envioDeMensagens (void *ptr)
     {
+        Controle *controle = (Controle*) ptr;   // Recebe as informações globais armazenadas
+                                                // na thread da main e passadas no ponteiro.
+
         std::cout << "Thread de envio de mensagens criada corretamente." << std::endl;
 
-        int *sair = (int*) ptr;
-
-        while (!(*sair))
+        while (!(controle->sair))   // Enquanto não for recebida a ordem de sair do programa
         {
+            pthread_mutex_lock(&controle->mutexFilaMensagens);  // Trava a fila de mensagens (controle de concorrência)
+
+            while (!(controle->filaMensagens.empty()))          // Enquanto a fila de mensagens contiver alguém
+            {
+                Mensagem m = controle->filaMensagens.front();             // Retirar primeira mensagem da fila e enviar
+                controle->filaMensagens.pop();
+
+                // Falta aqui o código que envia de fato uma mensagem.
+            }
+            pthread_mutex_unlock(&controle->mutexFilaMensagens);    // Destrava a fila de mensagens
         }
 
+        std::cout << "Thread envioDeMensagens encerrada." << std::endl;
         return NULL;
     }
 
@@ -47,10 +69,12 @@ namespace funcoesThread
     {
         std::cout << "Thread de recebimento de conexões criada corretamente." << std::endl;
         
-        Controle *controle = (Controle*) ptr;
+        const int porta = 2011;                 // Constante com o número da porta que será usada.
+        Controle *controle = (Controle*) ptr;   // Recebe as informações globais armazenadas
+                                                // na thread main e passadas no ponteiro.
 
-        int socketID;
-        sockaddr_in serv_addr;
+        int socketID;           // Identificador do socket que receberá as conexões.
+        sockaddr_in serv_addr;  // Cria um endereço para o servidor.
          
         // Abre canal de comunicação externa.
         socketID = socket(AF_INET, SOCK_STREAM, 0);
@@ -60,22 +84,24 @@ namespace funcoesThread
             return NULL;
         }
      
-        serv_addr.sin_family = AF_INET;            // Cria um endereço IPv4 local
-        serv_addr.sin_port = htons(2010);          // Determina uma porta para a comunicação
-        serv_addr.sin_addr.s_addr = INADDR_ANY;    // 
+        serv_addr.sin_family = AF_INET;            // Cria um endereço IPv4 local.
+        serv_addr.sin_port = htons(porta);         // Determina uma porta para a comunicação.
+        serv_addr.sin_addr.s_addr = INADDR_ANY;    // Recebe todos os pacotes da porta.
      
-        // Liga o canal criado ao endereço da máquina. Usa o sockaddr que foi setado acima
+        // Liga o canal criado ao endereço da máquina. Usa o sockaddr que foi criado acima.
         if (bind(socketID, (const sockaddr*) &serv_addr, sizeof(serv_addr)))
         {
-            std::cout << "Impossível abrir canal de comunicação externa. Encerrando a aplicação." << std::endl;
+            // Thread aborta se não puder receber conexões externas.
+            std::cout << "Impossível abrir canal de comunicação externa (bind). Encerrando a aplicação." << std::endl;
             return NULL;
         }
          
-        // Abre o canal para recepção de comunicações externas
+        // Abre o canal para recepção de comunicações externas.
         // Determina uma fila de tamanho até 2^16. Se houver 2^16 conexões na fila, novas conexões
         // serão descartadas e a aplicação cliente receberá um código de erro.
         if (listen(socketID, (1<<16)) < 0)
         {
+            // Thread aborta se não for possível ficar escutando no socket criado.
             std::cout << "Impossível receber conexões externas (listen). Encerrando a aplicação." << std::endl;
             return NULL;
         }
@@ -102,13 +128,39 @@ namespace funcoesThread
 
         // Para cada conexão recebida cria uma nova thread para lidar com ela e volta a esperar
         // nova conexão externa.
-        pthread_mutex_lock(&mutex);
-        while (!encerra)
+        pthread_mutex_lock(&controle->mutexEncerramento);
+        while (!controle->sair)
         {
-            pthread_mutex_unlock(&mutex);
+            pthread_mutex_unlock(&controle->mutexEncerramento);
+
             Cliente *cliente = new Cliente();
-            cliente->socketID = accept(socketID, (sockaddr*) &(cliente->addr), &(cliente->size));
-            if (cliente->socketID < 0)
+            cliente->socketEnvioID = accept(socketID, (sockaddr*) &(cliente->endereco), &(cliente->tamanho));
+            if (cliente->socketEnvioID < 0)
+            {
+                std::cout << "Falha na recepção de novas conexões. Encerrando thread. (accept)" << std::endl;
+                return NULL;
+            }
+
+            /*
+             * Problemas com a forma como deve ser feita a passagem dos dados.
+             * Preciso passar os dados que já tenho do cliente e, ao mesmo tempo, passar os dados globais do programa.
+             * Saída deve ser agrupar os dois em um outra estrutura só para essa transmissão.
+             */
+            if (pthread_create(&(cliente->thread), NULL, recebimentoDeMensagens, /******************************/))
+            {
+                std::cout << "Não é possível criar thread para o cliente. Ignorando conexão." << std::endl;
+            }
+            
+            pthread_mutex_lock(&controle->mutexEncerramento);
+        }
+        /*
+        pthread_mutex_lock(&controle->mutexEncerramento);
+        while (!controle->sair)
+        {
+            pthread_mutex_unlock(&controle->mutexEncerramento);
+            Cliente *cliente = new Cliente();
+            cliente->socketEnvioID = accept(socketID, (sockaddr*) &(cliente->addr), &(cliente->size));
+            if (cliente->socketEnvioID < 0)
             {
                 cout << "Impossível receber conexões externas (accept). Encerrando a aplicação." << std::endl;
                 return NULL;
@@ -117,10 +169,12 @@ namespace funcoesThread
             {
                 cout << "Impossível criar thread para tratar conexões externas. Ignorando conexão." << std::endl;
             }
-             pthread_mutex_lock(&mutex);
+             pthread_mutex_lock(&controle->mutexEncerramento);
         }
         close(socketID);
+        */
 
+        std::cout << "Thread recebimentoDeConexoes encerrada." << std::endl;
         return NULL;
     }
 
--- trunk/Servidor/source/main.cpp
+++ trunk/Servidor/source/main.cpp
@@ -1,3 +1,11 @@
+/*
+ * 
+ * Este arquivo contém o programa principal. Trata-se apenas da função main, que contém as instruções
+ * a serem executadas para a criação das threads e fica responsável apenas por verificar se o progra-
+ * ma deve ser fechado.
+ *
+ */
+
 #include <cstdio>
 #include <iostream>
 #include <cstring>
@@ -24,44 +32,55 @@
 #include <sys/ipc.h>
 #include <sys/types.h>
 
+// Incluindo a estrutura Controle, que guarda os dados comuns a todas as threads
 #include "../headers/controle"
+
+// Incluindo os protótipos da funções que representam as threads
 #include "../headers/prototipos"
 
 int main (int argc, char *argv[])
 {
-    pthread_t threadEnvio, threadConexao, threadRecebimento;
-    Controle controle(0); 
+    Controle controle(0);       // Cria a estrutura do tipo controle com as informações comuns
+                                // a todas as threads do programa.
 
-    pthread_mutex_init(&mutexEncerramento, NULL);
-    pthread_mutex_init(&mutexListaClientes, NULL);
+    pthread_t threadEnvio,      // Apesar de os nomes serem autoexplicativos, trata-se dos ids
+              threadConexao;    // das threads criadas pela main.
 
     std::cout << "Criando thread de envio de mensagens." << std::endl;    
-    if (pthread_create(&threadEnvio, NULL, funcoesThread::envioDeMensagens, (void*) &sair) != 0)
+    if (pthread_create(&threadEnvio, NULL, funcoesThread::envioDeMensagens, (void*) &controle) != 0)
     {
+        // Como não faz sentido um servidor que não pode enviar mensagens, o programa é abortado caso não
+        // seja possível iniciar a thread que envia as mensagens aos clientes.
         std::cout << "Erro na criação da thread que enviaria mensagens. Programa será abortado." << std::endl;
-        exit(1);
+        exit(1);    
     }
     
-    std::cout << "Criando thread de recebimento de mensagens." << std::endl;
-    if (pthread_create(&threadRecebimento, NULL, funcoesThread::recebimentoDeMensagens, (void*) &sair) != 0)
+    std::cout << "Criando thread para recebimento de novas conexões." << std::endl;
+    if (pthread_create(&threadConexao, NULL, funcoesThread::recebimentoDeConexoes, (void*) &controle) != 0)
     {
+        // Como não faz sentido um servidor que não pode receber mensagens ou iniciar conexões, o programa é 
+        // abortado caso não seja possível iniciar a thread que aguarda a conexão dos clientes.
         std::cout << "Erro na criação da thread que receberia conexoes. Programa será abortado." << std::endl;
         exit(1);
     }
     
-    while (!sair)
+    // Aguarda o comando que sai do programa. Inicialmente, para fins de teste, está como um char lido da
+    // entrada padrão, mas isso deve ser alterado para receber o comando de fechar da interface gráfica.
+    while (!controle.sair)
     {
         char c;
         
         std::cin >> c;        
         if (c == 's' || c == 'S')
         {
-            sair = 1;
+            controle.sair = 1;
         }
     }
 
+    std::cout << "Aguardando encerramento das threads." << std::endl;
+    // Aguarda o fim das threads do programa antes de encerrar.
     pthread_join(threadEnvio, NULL);
-    pthread_join(threadRecebimento, NULL);
+    pthread_join(threadConexao, NULL);
 
     return 0;
 }
